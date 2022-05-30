afb5cc6c09e6cb4248068d57c6f6ff5e2a0f0719
Sarder-Zilan <snasirzilan@gmail.com>
2022-05-30 11:39:57 +0600

    added a comment

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+# this is a model model
+
 # Create your models here
-
-
 class Product(models.Model):
 
     def __str__(self):
